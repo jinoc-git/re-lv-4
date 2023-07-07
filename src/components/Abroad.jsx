c4d162682fd3ddca6594cb213e9f3c82bb3ce90b
jinoc <choiae369@gmail.com>
2023-07-07 16:38:51 +0900

    Feat: 장르별 데이터 매핑, 초기 데이터 추가

--- src/components/Abroad.jsx
+++ src/components/Abroad.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Layout from './layout/Layout';
 import SearchForm from './form/SearchForm';
 import Contents from './Contents/Contents';
+import { useQuery } from '@tanstack/react-query'
+import { getPosts } from '../api/post'
 
 const Abroad = () => {
+  const { isLoading, isError, data } = useQuery(['posts'], getPosts);
+
   return (
     <Layout>
       <SearchForm />
-      <Contents />
+      <Contents posts={data} genre={'abroad'} />
     </Layout>
   );
 };
